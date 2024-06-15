81ae53aad63b1e6270ecc766917b6ba57ba79315
Utkarsh Paliwal <utkarshpaliwal1234@gmail.com>
2024-06-15 14:56:11 +0530

    server URL

--- frontend/src/Server_URL.js
+++ frontend/src/Server_URL.js
@@ -1,5 +1,6 @@
 // const Server_URL = import.meta.env.VITE_SERVER_URL
-const Server_URL = "http://localhost:8800"
+// const Server_URL = "http://localhost:8800"
+const Server_URL = ""
 // console.log(import.meta.env.VITE_SERVER_URL);
 // const Server_URL = "https://varta-app-api.vercel.app"
 export default Server_URL
